45f3d97eac40617fb7314cdb7a435f05500626c1
Zhuludkovr <82827317+NEXT-GM@users.noreply.github.com>
2021-11-18 19:24:19 +0300

    создание login страницы

--- borstie/src/views/Login.vue
+++ borstie/src/views/Login.vue
@@ -1,6 +1,19 @@
 <template>
 <div>
-    
+    <div class="parents">
+        <form class="main">
+        <div class="form-item">
+            <input type="text">
+        </div>
+        <div class="form-item">
+            <input type="text">
+        </div>
+        <div class="form-item">
+            <button>Войти</button>
+
+            </div>
+        </form>
+    </div>
 </div>
 </template>
 
@@ -10,8 +23,25 @@ export default {
 }
 </script>
 
-<style>
-.template{
-    background: linear-gradient();
-} 
+<style scoped>
+.parents{
+    width: 100%;
+    margin: 0%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.main{
+    width:430px;
+    height: 520px;
+    background: rgb(50, 100, 125);
+    margin-top: 150px;
+    border-radius: 10px;
+    padding: 40px 60px;
+}
+.form-item{
+    width: 100%;
+    height: 100px;
+}
+
 </style>
